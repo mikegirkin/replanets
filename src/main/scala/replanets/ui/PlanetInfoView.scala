86fc728d2f039d2d5ff68818ba7dfd335ef3e6dc
Mikhail Girkin <mgirkin@gilt.com>
2016-08-06 20:46:10 +0100

    Refactored planet info view

--- src/main/scala/replanets/ui/PlanetInfoView.scala
+++ src/main/scala/replanets/ui/PlanetInfoView.scala
@@ -1,10 +1,11 @@
 package replanets.ui
 
+import replanets.common.Constants
 import replanets.model.Game
 import replanets.ui.ScalafxHelpers.loadController
 
 import scalafx.scene.control.Label
-import scalafx.scene.layout.{Pane, VBox}
+import scalafx.scene.layout.{GridPane, Pane, VBox}
 import scalafxml.core.macros.sfxml
 /**
   * Created by mgirkin on 06/08/2016.
@@ -24,16 +25,21 @@ class PlanetInfoView(
   private val lblFcode: Label,
   private val lblClimate: Label,
   private val pnNatives: Pane,
-  private val pnColonists: Pane
+  private val pnColonists: Pane,
+  val lblColonistPopulation: Label,
+  val lblColonistTax: Label,
+  val lblColonistHappiness: Label,
+  val lblNatives: Label,
+  val lblGovernment: Label,
+  val lblPopulation: Label,
+  val lblTax: Label,
+  val lblHappiness: Label,
+
+  val gridColonists: GridPane,
+  val gridNatives: GridPane
 ) extends IPlanetInfoView {
 
   var game:Game = null
-
-  private val nativesView = loadController[INativesView]("/NativesView.fxml")
-  private val colonistsView = loadController[IColonistsView]("/ColonistsView.fxml")
-
-  pnNatives.children = nativesView.root
-  pnColonists.children = colonistsView.root
 
   override def setGameModel(model: Game): IPlanetInfoView ={
     this.game = model
@@ -41,6 +47,9 @@ class PlanetInfoView(
   }
 
   override def setPlanetId(turnId: Int, planetId: Int): Unit = {
+    gridNatives.visible = false
+    gridColonists.visible = false
+
     val data = game.turnSeverData(turnId)
     lblName.text = s"${game.map.planets(planetId).name} ($planetId)"
     lblWhen.text = "(now)"
@@ -48,8 +57,20 @@ class PlanetInfoView(
       lblOwner.text = game.races(p.ownerId - 1).shortname
       lblFcode.text = p.fcode
       lblClimate.text = p.temperature.toString
-      nativesView.setData(p)
-      colonistsView.setData(p)
+
+      if(p.nativeRace != 0) {
+        lblNatives.text = Constants.natives(p.nativeRace)
+        lblGovernment.text = Constants.nativeGovernments(p.nativeGovernment)
+        lblPopulation.text = s"${p.nativeClans} cl"
+        lblTax.text = s"${p.nativeTax} %"
+        lblHappiness.text = s"${p.nativeHappiness} %"
+        gridNatives.visible = true
+      }
+
+      lblColonistPopulation.text = s"${p.colonistClans} cl"
+      lblColonistTax.text = s"${p.colonistTax} %"
+      lblColonistHappiness.text = s"${p.colonistHappiness} %"
+      gridColonists.visible = true
     })
   }
 }
