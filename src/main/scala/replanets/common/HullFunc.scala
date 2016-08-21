d0cfcb30fed18262c49e8dd54f257a5c760dfab0
Mike Girkin <mikegirkin@gmail.com>
2016-08-21 13:18:59 +0100

    PHost hull functions

--- src/main/scala/replanets/common/HullFunc.scala
+++ src/main/scala/replanets/common/HullFunc.scala
@@ -6,6 +6,7 @@ class HullFunc protected (
   val hullFuncTxtCode: String
 )
 
+//THost
 case object Alchemy extends HullFunc(0, "Alchemy", "Alchemy")
 case object Refinery extends HullFunc(1, "Neutronic refinery", "Refinery")
 case object AdvancedRefinery extends HullFunc(2, "Advanced refinery", "AdvancedRefinery")
@@ -26,3 +27,32 @@ case object Cloak extends HullFunc(16, "Cloak", "Cloak")
 case object Bioscan extends HullFunc(17, "Bioscanner", "Bioscan")
 case object GloryDeviceLowDamage extends HullFunc(18, "Advanced glory device", "GloryDeviceLowDamage")
 case object GloryDeviceHighDamage extends HullFunc(19, "Glory device", "GloryDeviceHighDamage")
+
+//Phost 4.0+
+case object Unclonable extends HullFunc(20, "Unclonable", "Unclonable")
+case object CloneOnce extends HullFunc(21, "Clonable only once", "CloneOnce")
+case object Ungiveable extends HullFunc(22, "Cannot be given away", "Ungiveable")
+case object GiveOnce extends HullFunc(23, "Can be given away once", "GiveOnce")
+case object Level2Tow extends HullFunc(24, "Stronger tractor beam", "Level2Tow")
+
+//Phost 4.0i+
+case object Tow extends HullFunc(25, "Tractor beam", "Tow")
+case object ChunnelSelf extends HullFunc(26, "Chunnel itself", "ChunnelSelf")
+case object ChunnelOthers extends HullFunc(27, "Chunnel others", "ChunnelOthers")
+case object ChunnelTarget extends HullFunc(28, "Chunnel target", "ChunnelTarget")
+case object PlanetImmunity extends HullFunc(29, "Immune to ATT/NUK", "PlanetImmunity")
+case object OreCondenser extends HullFunc(30, "Ore condenser", "OreCondenser")
+case object Boarding extends HullFunc(31, "Tow-capture", "Boarding")
+case object AntiCloakImmunity extends HullFunc(32, "Immune to anti-cloak", "AntiCloakImmunity")
+case object Academy extends HullFunc(33, "Academy", "Academy")
+case object Repairs extends HullFunc(34, "Repairs", "Repairs")
+case object FullWeaponry extends HullFunc(35, "Full weaponry", "FullWeaponry")
+case object HardenedEngines extends HullFunc(36, "Hardened engines", "HardenedEngines")
+
+//Phost 4.0j+
+case object Commander extends HullFunc(37, "Commander", "Commander")
+case object IonShield extends HullFunc(38, "Ion shield", "IonShield")
+case object HardenedCloak extends HullFunc(39, "Hardened cloak", "HardenedCloak")
+
+//Phost 4.0k+
+case object AdvancedAntiCloak extends HullFunc(40, "Advanced anti-cloak", "AdvancedAntiCloak")
